94649b4442b80e8c7e3236aae8a26950f42080e1
Tobias Stenzel <tobias.stenzel@tum.de>
2014-04-10 15:16:57 +0200

    temporary fix for redmine_group_owners exception

---
+++ playbook-redmine/roles/redmine_plugins/files/group_owners_patches.rb
@@ -0,0 +1,6 @@
+#patches
+require 'group_owners/patches/group_patch'
+require 'group_owners/patches/user_patch'
+require 'group_owners/patches/groups_helper_patch'
+require 'group_owners/patches/groups_controller_patch'
+
